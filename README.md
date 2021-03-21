206a8b2a4bca586daa2dbcabbf24fca1798b60ef
Lorenzo Breda <lorenzo@lbreda.com>
2021-03-21 15:56:29 +0100

    :tada: First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# MovieDB Bot
+
+A simple Telegram inline bot to get informations from The Movie Database. It runs as [@moviedbbot](https://t.me/moviedbbot) on Telegram.
+
+## How to run it:
+Clone this repository, copi `.env.example` in `.env`, set your API key for Telegram and The Movie Database, and run:
+
+```bash
+npm install
+node index.js
+```
+
+You need NodeJS.
+
+## How to use it
+Make a inline query to the bot to search movies, tv series and people.
---
+++ index.js
@@ -0,0 +1,131 @@
+const MovieDB = require('node-themoviedb');
+const { Telegraf } = require('telegraf')
+const emoji = require('node-emoji')
+require('dotenv').config()
+const mdb = new MovieDB(process.env.TMD_TOKEN, {})
+const bot = new Telegraf(process.env.TELEGRAM_TOKEN)
+
+// Gets item image link
+// Caveat: between '[' and ']' in the link U+200C zero-width character
+let getItemImageLink = (item) => {
+  return `[‌‌](http://image.tmdb.org/t/p/original${item.poster_path || item.profile_path})`
+}
+
+// Gets item production countries flags
+let getProductionFlags = (item) => {
+  return item.production_countries ? item.production_countries.map(c => emoji.get(`flag-${c.iso_3166_1.toLowerCase()}`)) : ''
+}
+
+// Gets link to a person
+let getPersonLink = (person) => {
+  return `[${person.name}](https://www.themoviedb.org/person/${person.id})`
+}
+
+// Parses a TMDB multi research item to get a markdown description
+let tmdbMultiItemToMarkdown = async (item) => {
+  if (item.media_type === 'tv') {
+    let details = (await mdb.tv.getDetails({ pathParameters: { tv_id: item.id } })).data
+
+    let result = getItemImageLink(item)
+    result += `*${item.title || item.name}* ${getProductionFlags(details)}\n\n`
+
+    result += `*Original title*: [${item.original_name || item.name}](https://www.themoviedb.org/tv/${item.id})\n`
+    if (details.first_air_date) {
+      result += ` (${details.first_air_date} - {${details.last_air_date || '?'}})`
+    }
+    result += "\n"
+    if (details.created_by) {
+      result += `*Created by*: ${details.created_by.map(p => getPersonLink(p))}\n`
+    }
+    if (details.number_of_seasons) {
+      result += `*Seasons*: ${details.number_of_seasons}\n`
+    }
+    if (details.number_of_episodes) {
+      result += `*Episodes*: ${details.number_of_episodes}\n`
+    }
+
+    result += `\n${item.overview}`
+    return result
+  }
+
+  if (item.media_type === 'movie') {
+    let details = (await mdb.movie.getDetails({ pathParameters: { movie_id: item.id } })).data
+
+    let result = getItemImageLink(item)
+    result += `*${item.title}* ${getProductionFlags(details)}\n\n`
+
+    result += `*Original title*: [${item.original_title || item.title}](https://www.themoviedb.org/movie/${item.id})`
+    if (details.release_date) {
+      result += ` (${details.release_date.substr(0, 4)})`
+    }
+    result += "\n"
+    if (details.runtime) {
+      result += `*Runtime*: ${details.runtime}mins`
+    }
+
+    if (details.credits && details.credits.crew) {
+      result += `*Directed by*: ${details.credits.crew.filter(p => p.department === 'Directing').map(p => getPersonLink(p))}\n`
+      result += `*Story by*: ${details.credits.crew.filter(p => p.department === 'Writing').map(p => getPersonLink(p))}\n`
+    }
+
+    result += `\n*Other sites:* [Letterboxd](http://letterboxd.com/tmdb/{${item.id})`
+    if (details.imdb_id) {
+      result += `, [IMDb](https://www.imdb.com/title/${details.imdb_id})`
+    }
+    result += "\n"
+
+    result += `\n${item.overview}`
+    return result
+  }
+
+  if (item.media_type === 'person') {
+    let details = (await mdb.person.getDetails({ pathParameters: { person_id: item.id } })).data
+
+    let result = getItemImageLink(item)
+    result += `*${item.name}*\n\n`
+
+    result += `[TMDB page](https://www.themoviedb.org/person/${item.id})\n\n`
+
+    if (details.birthday) {
+      result += `*Birth:* ${details.birthday}` + (details.place_of_birth ? ` (${details.place_of_birth})` : '') + "\n"
+    }
+    if (details.deathday) {
+      result += `*Death:* ${details.deathday}\n`
+    }
+
+    if (details.imdb_id) {
+      result += `Other sites: [IMDb](https://www.imdb.com/name/${details.imdb_id})`
+    }
+
+    result += `\n*Known for:* ${item.known_for.map(el => el.name || el.title).join(', ')}`
+
+    return result
+  }
+}
+
+// Parses TMDB data to get a Telegram inline query response
+let tmdbSearchResultsToQueryResponse = (results) => results.map(result => (async () => {
+  let message_text = await tmdbMultiItemToMarkdown(result)
+  return {
+    type: 'article',
+    id: result.media_type + result.id,
+    title: `[${result.media_type}] ${result.name || result.title}`,
+    description: result.overview,
+    thumb_url: `http://image.tmdb.org/t/p/w92${result.profile_path || result.poster_path}`,
+    input_message_content: {
+      'message_text': message_text,
+      'parse_mode': 'Markdown'
+    }
+  }
+})())
+
+bot.on('inline_query', async (ctx) => {
+  let response = [];
+  if (ctx.inlineQuery.query) {
+    let results = await mdb.search.multi({ query: { query: ctx.inlineQuery.query } });
+    response = await Promise.all(tmdbSearchResultsToQueryResponse(results.data.results))
+  }
+  return await ctx.answerInlineQuery(response)
+})
+
+bot.launch()
